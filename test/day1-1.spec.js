56b298e27c09d54f3e892b268893e28219a49e6e
Jared Siirila <jsiirila@ebsco.com>
2017-12-04 16:39:57 -0500

    initial commit

---
+++ test/day1-1.spec.js
@@ -0,0 +1,8 @@
+import { matchingDigitsSum } from '../src/day1-1';
+
+describe('Sum of matching digits', () => {
+	it('Exists', () => {
+		expect(true).toBeTruthy();
+		expect(matchingDigitsSum).toEqual(jasmine.anything());
+	});
+});
