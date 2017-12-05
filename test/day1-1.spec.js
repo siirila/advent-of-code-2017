79c7c8d137c5c4f1964e5bf9f8317a8f1571c5c7
Jared Siirila <jsiirila@ebsco.com>
2017-12-05 07:45:46 -0500

    day 1 first puzzle

--- test/day1-1.spec.js
+++ test/day1-1.spec.js
@@ -1,8 +1,16 @@
 import { matchingDigitsSum } from '../src/day1-1';
 
 describe('Sum of matching digits', () => {
-	it('Exists', () => {
-		expect(true).toBeTruthy();
-		expect(matchingDigitsSum).toEqual(jasmine.anything());
+	it('Returns 3', () => {
+		expect(matchingDigitsSum('1122')).toEqual(3);
+	});
+	it('Returns 4', () => {
+		expect(matchingDigitsSum('1111')).toEqual(4);
+	});
+	it('Returns 0', () => {
+		expect(matchingDigitsSum('1234')).toEqual(0);
+	});
+	it('Returns 9', () => {
+		expect(matchingDigitsSum('91212129')).toEqual(9);
 	});
 });
